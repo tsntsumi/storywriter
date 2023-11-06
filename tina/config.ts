3489e0c3eb3b69c05108ad32397faff070896e50
tsntsumi <tsutsumi.kikuo@gmail.com>
2023-11-07 05:02:29 +0900

    Update to handle MDX

--- tina/config.ts
+++ tina/config.ts
@@ -1,41 +1,42 @@
-import { defineConfig } from "tinacms";
+import { defineConfig } from 'tinacms'
 
 // Your hosting provider likely exposes this as an environment variable
-const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || 'main'
 
 export default defineConfig({
   branch,
-  clientId: "ae869f0a-53b7-4cbd-8cdb-767e79d7d2ca", // Get this from tina.io
-  token: "017dba4398e716e84cf8e484d2dc0d424e44f8ee", // Get this from tina.io
+  clientId: 'ae869f0a-53b7-4cbd-8cdb-767e79d7d2ca', // Get this from tina.io
+  token: '017dba4398e716e84cf8e484d2dc0d424e44f8ee', // Get this from tina.io
 
   build: {
-    outputFolder: "admin",
-    publicFolder: "public",
+    outputFolder: 'admin',
+    publicFolder: 'public',
   },
   media: {
     tina: {
-      mediaRoot: "",
-      publicFolder: "public",
+      mediaRoot: '',
+      publicFolder: 'public',
     },
   },
   schema: {
     collections: [
       {
-        name: "post",
-        label: "Posts",
-        path: "content/posts",
+        name: 'post',
+        label: 'Posts',
+        path: 'content/posts',
+        format: 'mdx',
         fields: [
           {
-            type: "string",
-            name: "title",
-            label: "Title",
+            type: 'string',
+            name: 'title',
+            label: 'Title',
             isTitle: true,
             required: true,
           },
           {
-            type: "rich-text",
-            name: "body",
-            label: "Body",
+            type: 'rich-text',
+            name: 'body',
+            label: 'Body',
             isBody: true,
           },
         ],
@@ -46,4 +47,4 @@ export default defineConfig({
       },
     ],
   },
-});
+})
