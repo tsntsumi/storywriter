7daff12952a48a271ad89270d7f859af9c644ebc
tsntsumi <tsutsumi.kikuo@gmail.com>
2023-11-13 04:27:49 +0900

    Remove unused import

--- src/app/page.js
+++ src/app/page.js
@@ -11,7 +11,6 @@ import Brands from "@/components/Brands"
 import Feature from "@/components/Features"
 import About from "@/components/About"
 import CTA from "@/components/CTA"
-import BlogEntryCollection from "@/components/Blog/Collection"
 
 import firebaseConfig from "@/firebase-config"
 
